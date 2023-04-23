fd626c64119569fc77f5a279140c7494d2735fe1
‘li’ <3327084764@qq.com>
2023-04-23 23:44:54 +0800

    选举完善

--- village_front/src/constants/index.js
+++ village_front/src/constants/index.js
@@ -1,76 +1,155 @@
 export const config = {
-        NATIONLIST: [
-            {id: 1,info: '汉族',value: 1},{id: 2,info: '壮族',value: 2},{id: 3,info: '满族',value: 3},
-            {id: 4,info: '回族',value: 4},{id: 5,info: '苗族',value: 5},{id: 6,info: '维吾尔族',value: 6},
-            {id: 7,info: '土家族',value: 7},{id: 8,info: '彝族',value: 8},{id: 9,info: '蒙古族',value: 9},
-            {id: 10,info: '藏族',value: 10},{id: 11,info: '布依族',value: 11},{id: 12,info: '侗族',value: 12},
-            {id: 13,info: '瑶族',value: 13},{id: 14,info: '朝鲜族',value: 14},{id: 15,info: '白族',value: 15},
-            {id: 16,info: '哈尼族',value: 16}, {id: 17,info: '哈萨克族',value: 17},{d: 18,info: '黎族', value: 18},
-            {id: 19,info: '傣族',value: 19},{id: 20,info: '畲族',value: 20},{id: 21,info: '傈僳族',value: 21},
-            {id: 22,info: '仡佬族',value: 22},{id: 23,info: '东乡族',value: 23},{id: 24, info: '高山族', value: 24},
-            {id: 25,info: '拉祜族',value: 25},{id: 26,info: '水族',value: 26},{id: 27,info: '佤族',value: 27 },
-            {id: 28,info: '纳西族',value: 28 },{id: 29,info: '羌族',value: 29},{id: 30,info: '土族',value: 30},
-            {id: 31,info: '仫佬族',value: 31},{id: 32,info: '锡伯族',value: 32},{id: 33,info: '柯尔克孜族',value: 33},
-            {id: 34,info: '达斡尔族', value: 34},{id: 35, info: '景颇族', value: 35 }, {id: 36,info: '毛南族',value: 36},
-            {id: 37,info: '撒拉族', value: 37}, {id: 38, info: '布朗族',value: 38},{ id: 39,info: '塔吉克族',value: 39},
-            {id: 40,info: '阿昌族',value: 40},{id: 41,info: '普米族', value: 41},{id: 42,info: '鄂温克族', value: 42 },
-            {id: 43,info: '怒族',value: 43}, {id: 44, info: '京族',value: 44}, {id: 45,info: '基诺族',value: 45},
-            {id: 46, info: '德昂族', value: 46},{id: 47,info: '保安族',value: 47}, {id: 48,info: '俄罗斯族',value: 48},
-            {id: 49,info: '裕固族',value: 49}, {id: 50,info: '乌孜别克族',value: 50},{id: 51,info: '门巴族', value: 51},
-            {id: 52,info: '鄂伦春族', value: 52 },{id: 53,info: '独龙族',value: 53},{id: 54,info: '塔塔尔族',value: 54},
-            {id: 55,info: '赫哲族',value: 55},{ id: 56,info: '珞巴族',value: 56}
-        ],
-    
-        EDULIST: [
-            {id: 2,label: '学龄前'},{id: 3,label: '文盲或半文盲'},{id: 4,label: '小学'},
-            {id: 5,label: '初中' },{id: 6,label: '高中'},{id: 7,label: '技工学校'},{id: 8,label: '中等专业学校'},
-            {id: 9, label: '大专'},{id: 10,label: '大学专科和专科学校'},{id: 11,label: '大学本科'},{id: 12,label: '研究生'},
-            {id: 13,label: '其他'}
-        ],
-    
-        POLICYLIST: [
-                {id: 1,label: '中国共产党党员'},{id: 2,label: '九三学学社社员'},{id: 3,label: '台湾民主自治同盟盟员'},
-                {id: 4,label: '无党派民主人士'},{id: 5,label: '群众'},{id: 6,label: '中国共产党预备党员'},
-                {id: 7,label: '中国共产主义青年团团员'},{id: 8,label: '中国国民党革命委员会会员'},{id: 9,label: '中国民主同盟'},
-                {id: 10,label: '中国民主建国会会员'},{id: 11,label: '中国民主促进会会员'},{id: 12,label: '中国农工民主党党员'},
-                {id: 13,label: '中国致公党党员'},{id: 14,label: '其他'}
-            ],
-        RELATION: [
-                    {
-                            value: '妻子',
-                            label: '妻子',
-                    },
-                    {
-                            value: '儿子',
-                            label: '儿子',
-                    },
-                    {
-                            value: '女儿',
-                            label: '女儿',
-                    },
-                    {
-                            value: '父亲',
-                            label: '父亲',
-                    },
-                    {
-                            value: '母亲',
-                            label: '母亲',
-                    },
-                    {
-                            value: '爷爷',
-                            label: '爷爷',
-                    },
-                    {
-                            value: '奶奶',
-                            label: '奶奶',
-                    },
-            ],
-        VOTETYPE:[
-        {label:'村长',value:'村长'},
-        {label:'村民委员会主任',value:'村民委员会主任'},
-        {label:'村民委员会副主任',value:'村民委员会副主任'},
-        {label:'党总支书记',value:'党总支书记'},
-        {label:'小组长',value:'小组长'}
-        ]
-    }
-    
+  NATIONLIST: [
+    { id: 1, info: "汉族", value: 1 },
+    { id: 2, info: "壮族", value: 2 },
+    { id: 3, info: "满族", value: 3 },
+    { id: 4, info: "回族", value: 4 },
+    { id: 5, info: "苗族", value: 5 },
+    { id: 6, info: "维吾尔族", value: 6 },
+    { id: 7, info: "土家族", value: 7 },
+    { id: 8, info: "彝族", value: 8 },
+    { id: 9, info: "蒙古族", value: 9 },
+    { id: 10, info: "藏族", value: 10 },
+    { id: 11, info: "布依族", value: 11 },
+    { id: 12, info: "侗族", value: 12 },
+    { id: 13, info: "瑶族", value: 13 },
+    { id: 14, info: "朝鲜族", value: 14 },
+    { id: 15, info: "白族", value: 15 },
+    { id: 16, info: "哈尼族", value: 16 },
+    { id: 17, info: "哈萨克族", value: 17 },
+    { d: 18, info: "黎族", value: 18 },
+    { id: 19, info: "傣族", value: 19 },
+    { id: 20, info: "畲族", value: 20 },
+    { id: 21, info: "傈僳族", value: 21 },
+    { id: 22, info: "仡佬族", value: 22 },
+    { id: 23, info: "东乡族", value: 23 },
+    { id: 24, info: "高山族", value: 24 },
+    { id: 25, info: "拉祜族", value: 25 },
+    { id: 26, info: "水族", value: 26 },
+    { id: 27, info: "佤族", value: 27 },
+    { id: 28, info: "纳西族", value: 28 },
+    { id: 29, info: "羌族", value: 29 },
+    { id: 30, info: "土族", value: 30 },
+    { id: 31, info: "仫佬族", value: 31 },
+    { id: 32, info: "锡伯族", value: 32 },
+    { id: 33, info: "柯尔克孜族", value: 33 },
+    { id: 34, info: "达斡尔族", value: 34 },
+    { id: 35, info: "景颇族", value: 35 },
+    { id: 36, info: "毛南族", value: 36 },
+    { id: 37, info: "撒拉族", value: 37 },
+    { id: 38, info: "布朗族", value: 38 },
+    { id: 39, info: "塔吉克族", value: 39 },
+    { id: 40, info: "阿昌族", value: 40 },
+    { id: 41, info: "普米族", value: 41 },
+    { id: 42, info: "鄂温克族", value: 42 },
+    { id: 43, info: "怒族", value: 43 },
+    { id: 44, info: "京族", value: 44 },
+    { id: 45, info: "基诺族", value: 45 },
+    { id: 46, info: "德昂族", value: 46 },
+    { id: 47, info: "保安族", value: 47 },
+    { id: 48, info: "俄罗斯族", value: 48 },
+    { id: 49, info: "裕固族", value: 49 },
+    { id: 50, info: "乌孜别克族", value: 50 },
+    { id: 51, info: "门巴族", value: 51 },
+    { id: 52, info: "鄂伦春族", value: 52 },
+    { id: 53, info: "独龙族", value: 53 },
+    { id: 54, info: "塔塔尔族", value: 54 },
+    { id: 55, info: "赫哲族", value: 55 },
+    { id: 56, info: "珞巴族", value: 56 },
+  ],
+
+  EDULIST: [
+    { id: 2, label: "学龄前" },
+    { id: 3, label: "文盲或半文盲" },
+    { id: 4, label: "小学" },
+    { id: 5, label: "初中" },
+    { id: 6, label: "高中" },
+    { id: 7, label: "技工学校" },
+    { id: 8, label: "中等专业学校" },
+    { id: 9, label: "大专" },
+    { id: 10, label: "大学专科和专科学校" },
+    { id: 11, label: "大学本科" },
+    { id: 12, label: "研究生" },
+    { id: 13, label: "其他" },
+  ],
+
+  POLICYLIST: [
+    { id: 1, label: "中国共产党党员" },
+    { id: 2, label: "九三学学社社员" },
+    { id: 3, label: "台湾民主自治同盟盟员" },
+    { id: 4, label: "无党派民主人士" },
+    { id: 5, label: "群众" },
+    { id: 6, label: "中国共产党预备党员" },
+    { id: 7, label: "中国共产主义青年团团员" },
+    { id: 8, label: "中国国民党革命委员会会员" },
+    { id: 9, label: "中国民主同盟" },
+    { id: 10, label: "中国民主建国会会员" },
+    { id: 11, label: "中国民主促进会会员" },
+    { id: 12, label: "中国农工民主党党员" },
+    { id: 13, label: "中国致公党党员" },
+    { id: 14, label: "其他" },
+  ],
+  RELATION: [
+    {
+      value: "妻子",
+      label: "妻子",
+    },
+    {
+      value: "儿子",
+      label: "儿子",
+    },
+    {
+      value: "女儿",
+      label: "女儿",
+    },
+    {
+      value: "父亲",
+      label: "父亲",
+    },
+    {
+      value: "母亲",
+      label: "母亲",
+    },
+    {
+      value: "爷爷",
+      label: "爷爷",
+    },
+    {
+      value: "奶奶",
+      label: "奶奶",
+    },
+  ],
+  VOTETYPE: [
+    { label: "村长", value: "村长" },
+    { label: "村民委员会主任", value: "村民委员会主任" },
+    { label: "村民委员会副主任", value: "村民委员会副主任" },
+    { label: "党总支书记", value: "党总支书记" },
+    { label: "小组长", value: "小组长" },
+  ],
+  INTRODUCE: [
+    {
+      title: "荒田概况",
+      url: "../assets/img/01.jpg",
+      content:
+        "荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。",
+    },
+    {
+      title: "荒田概况",
+      url: "../assets/img/01.jpg",
+      content:
+        "荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。",
+    },
+    {
+      title: "荒田概况",
+      url: "../assets/img/01.jpg",
+      content:
+        "荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。",
+    },
+    {
+      title: "荒田概况",
+      url: "../assets/img/01.jpg",
+      content:
+        "荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。",
+    },
+  ],
+};
--- village_front/src/views/front/frontHome.vue
+++ village_front/src/views/front/frontHome.vue
@@ -1,40 +1,23 @@
 <template>
-    <div class="wrapper">
-      <div class="upper">
-        <div class="upper-title">走进荒田</div>
-        <div class="upper-content" v-for="item in data.introduceList">
-          <div class="upper-content_text">
-            <span>{{ item.content }}</span>
-            <div class="upper-btns">
-              <div class="upper-btn1">
-                <el-button style=" margin-left: 10px;" type="primary" icon="el-icon-edit" circle></el-button>
-                <div>荒田概况</div>
-              </div>
-              <div class="upper-btn1">
-                <el-button style=" margin-left: 10px;" type="success" icon="el-icon-edit" circle></el-button>
-                <div>特色产业</div>
-              </div>
-              <div class="upper-btn1">
-                <el-button style=" margin-left: 10px;" type="warning" icon="el-icon-edit" circle></el-button>
-                <div>基层组织</div>
-              </div>
-              <div class="upper-btn1">
-                <el-button style=" margin-left: 10px;" type="danger" icon="el-icon-edit" circle></el-button>
-                <div>文化教育</div>
-              </div>
-            </div>
-          </div>
-          <div class="upper-content_img">
-            <el-image
-              :src="item.url"
-              fit="fill">
-            </el-image>
-          </div>
+  <div class="wrapper">
+    <div class="upper">
+      <div class="upper-title">走进荒田</div>
+      <div class="upper-content" v-for="item in data.introduceList">
+        <div class="upper-content_text">
+          <span>{{ item.content }}</span>
+        </div>
+        <div class="upper-content_img">
+          <el-image
+            src="https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg"
+            fit="fill"
+          >
+          </el-image>
         </div>
       </div>
-      <hr>
-      <div class="bottom">
-        <div class="left">
+    </div>
+    <hr />
+    <div class="bottom">
+      <div class="left">
         <div class="swiper">
           <el-carousel :interval="5000" arrow="always" height="200px">
             <el-carousel-item v-for="item in 3" :key="item">
@@ -49,185 +32,190 @@
           type="card"
           class="demo-tabs"
           @tab-click="handleClick"
-          style="height: 250px;"
+          style="height: 250px"
         >
           <el-tab-pane label="乡村要闻" name="first">
             <div class="title">乡村要闻</div>
-            <hr>
-              <li v-for="item in data.villageNews" :index="item.id" :key="item.id">
-                <span>{{ item.title }}</span>
-              </li>
+            <hr />
+            <li
+              v-for="item in data.villageNews"
+              :index="item.id"
+              :key="item.id"
+            >
+              <span>{{ item.title }}</span>
+            </li>
           </el-tab-pane>
           <el-tab-pane label="凤庆县要闻" name="second">
             <div class="title">凤庆县要闻</div>
-            <hr>
-              <li v-for="item in data.countyNews" :index="item.id" :key="item.id">
-                <span>{{ item.title }}</span>
-              </li>
+            <hr />
+            <li v-for="item in data.countyNews" :index="item.id" :key="item.id">
+              <span>{{ item.title }}</span>
+            </li>
           </el-tab-pane>
           <el-tab-pane label="森林防火" name="third">
             <div class="title">森林防火</div>
-            <hr>
-              <li v-for="item in data.fileList" :index="item.id" :key="item.id">
-                <span>{{ item.title }}</span>
-              </li>
+            <hr />
+            <li v-for="item in data.fileList" :index="item.id" :key="item.id">
+              <span>{{ item.title }}</span>
+            </li>
           </el-tab-pane>
           <el-tab-pane label="法律法规" name="four">
             <div class="title">法律法规</div>
-            <hr>
-              <li v-for="item in data.lawList" :index="item.id" :key="item.id">
-                <span>{{ item.title }}</span>
-              </li>
+            <hr />
+            <li v-for="item in data.lawList" :index="item.id" :key="item.id">
+              <span>{{ item.title }}</span>
+            </li>
           </el-tab-pane>
         </el-tabs>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script setup>
-import { reactive,ref ,onMounted } from 'vue';
-import request from '../../request/request'
-const activeName = ref('first')
+import { reactive, ref, onMounted } from "vue";
+import request from "../../request/request";
+const activeName = ref("first");
 const data = reactive({
-    villageNews:[],
-    countyNews:[],
-    fileList:[],
-    lawList:[],
-    introduceList: [
-            { url:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',label : '荒田概况' , content : '荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。'}
-        ]
-  })
- const handleClick = (val)=>{
+  villageNews: [],
+  countyNews: [],
+  fileList: [],
+  lawList: [],
+  introduceList: [
+    {
+      url: "../../assets/img/gaikuang.jpg",
+      label: "荒田概况",
+      content:
+        "荒田村隶属于云南省临沧市凤庆县雪山镇，地处雪山镇东北边，东邻中山村，南邻兴街村，西邻三岔河镇光华村，北邻三岔河镇涌金村，到镇道路为土路，交通方便。该村距雪山镇政府所在地23公里，距凤庆县县城108公里。该村辖渔塘、田坝等15个村民小组，从事第一产业人数943人。全村国土面积16.3平方公里，海拔1850米，年平均气温23℃，年降水量1300毫米，适合种植水稻、玉米等农作物。农民收入以种植业为主。",
+    },
+  ],
+});
+const handleClick = (val) => {
   // console.log(val);
- }
- onMounted (()=>{
-    load()
- }) 
- const load = () => {
-  request.get("/api/task/findPage",{
-    params: {
-      pageNum: 1,
-      pageSize: 100,
-      title: '',
-      noticeType: ''
-    }
-  }).then(res=>{
-    const lists = res.data.data
-    lists.map(v=>{
-      switch (v.noticeType){
-        case '乡村要闻':
-          data.villageNews.push(v)
-          break
-        case '凤庆县要闻':
-          data.countyNews.push(v)
-          break
-        case '森林防火':
-          data.fileList.push(v)
-          break
-        case '法律法规':
-          data.lawList.push(v)
-          break
-      }
+};
+onMounted(() => {
+  load();
+});
+const load = () => {
+  request
+    .get("/api/task/findPage", {
+      params: {
+        pageNum: 1,
+        pageSize: 100,
+        title: "",
+        noticeType: "",
+      },
     })
-  })
-}
+    .then((res) => {
+      const lists = res.data.data;
+      lists.map((v) => {
+        switch (v.noticeType) {
+          case "乡村要闻":
+            data.villageNews.push(v);
+            break;
+          case "凤庆县要闻":
+            data.countyNews.push(v);
+            break;
+          case "森林防火":
+            data.fileList.push(v);
+            break;
+          case "法律法规":
+            data.lawList.push(v);
+            break;
+        }
+      });
+    });
+};
 </script>
 
 <style lang="less" scoped>
-.wrapper{
-    
-    background-color: #ffffffeb;
-    height: calc(100vh - 140px);
-    overflow: auto;
-    .upper{
-      .upper-title{
-        background-color: #92C4F8;
-        height: 40px;
-        line-height: 40px;
-      }
-      .upper-content{
-        margin: 10px;
-        display: grid;
-        grid-template-columns: 2fr 1fr;
-        gap: 20px;
-        .upper-content_text{
-          position:relative;
-          .upper-btns{
-            display: flex;
-            position: absolute;
-	          bottom: 10px;
-            left: 10px;
-            .upper-btn1{
-              margin-left: 20px;
-            }
+.wrapper {
+  background-color: #ffffffeb;
+  height: calc(100vh - 140px);
+  overflow: auto;
+  .upper {
+    .upper-title {
+      background-color: #92c4f8;
+      height: 40px;
+      line-height: 40px;
+    }
+    .upper-content {
+      margin: 10px;
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+      gap: 20px;
+      .upper-content_text {
+        position: relative;
+        .upper-btns {
+          display: flex;
+          position: absolute;
+          bottom: 10px;
+          left: 10px;
+          .upper-btn1 {
+            margin-left: 20px;
           }
         }
       }
     }
-    .bottom{
-      display: flex;
-      justify-content: space-around;
-      margin-top: 10px;
-      .left{
-    width: 50%;
-    .swiper{
-      width: 100%;
-      height: 200px;
-      background-color: rgb(102, 205, 86);
+  }
+  .bottom {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 10px;
+    .left {
+      width: 50%;
+      .swiper {
+        width: 100%;
+        height: 200px;
+        background-color: rgb(102, 205, 86);
         .el-carousel__item h3 {
           text-align: center;
         }
+      }
     }
-    
-  }
-  .right{
-    width: 48%;
+    .right {
+      width: 48%;
       margin-top: 10px;
       padding: 0;
-      .title{
+      .title {
         font-size: large;
         width: 100px;
         text-align: center;
         background-color: #79bbff;
       }
-      li{
+      li {
         height: 24px;
         list-style: none;
-        text-overflow:ellipsis; 
-        overflow: hidden; 
+        text-overflow: ellipsis;
+        overflow: hidden;
         a {
-                //去掉下换线
-                text-decoration: none;
-                color:rgb(94, 92, 92);
-          }
+          //去掉下换线
+          text-decoration: none;
+          color: rgb(94, 92, 92);
+        }
       }
-       
-    
-    .hot{
-      margin-top: 10px;
-      .title{
-        font-size: large;
-        width: 100px;
-        text-align: center;
-        background-color: #f05431;
-      }
-      li{
-        height: 24px;
-        list-style: none;
-        text-overflow:ellipsis; 
-        overflow: hidden; 
-        a {
-                //去掉下换线
-                text-decoration: none;
-                color:rgb(94, 92, 92);
+
+      .hot {
+        margin-top: 10px;
+        .title {
+          font-size: large;
+          width: 100px;
+          text-align: center;
+          background-color: #f05431;
+        }
+        li {
+          height: 24px;
+          list-style: none;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          a {
+            //去掉下换线
+            text-decoration: none;
+            color: rgb(94, 92, 92);
           }
+        }
       }
     }
   }
-    }
-
 }
-  
-
 </style>
